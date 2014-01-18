82792ed820d1547d842bf735ddb53a02e089c53b
jesajx <johannes.olegard@olissoft.com>
2014-01-18 16:59:49 +0100

    Added opts. (breaking main).

--- src/main.hs
+++ src/main.hs
@@ -11,13 +11,46 @@ import Text.ParserCombinators.Parsec (parse)
 import qualified Data.List as L
 import Data.Time
 
--- TODO opts?
---  --name=expr : e.g. --name="agroup.*", --name="*.mytask", etc.
---  --notname=expr : e.g. --notname "mytask"
---  --with-complete : include completed tasks
---  --without-complete : don't include completed tasks (default?)
+data Options = Options
+    { nameExprs :: [(String,Bool)]
+    , stressExprs :: [(Int, Ordering)]
+    , datetimeExpr :: [(datetime, Ordering)]  -- TODO intervals?
+    }
+
+-- TODO use othertype then Ordering. to implement >=, <= and <> ?
+
+defaultOptions now = Options
+    { nameExpr = []
+    , stressExpr = []
+    , datetimeExpr :: [(now, GT)]   -- EQ too?
+    }
+
+options :: [OptDescr (Options -> Options)]
+options =
+    [ Option ['n'] ["name"] (ReqArg \n -> (n, True))
+        "Expression to whitelist full tasknames. May be used multiple times."
+    , Option ['m'] ["notname"] (ReqArg \n -> (n, False))
+        "Expression to blacklist full tasknames. May be used multiple times."
+    , Option ['s'] ["stress"] (ReqArg parseOptStress)
+        "filter stress."
+    , Option ['d'] ["datetime"] (ReqArg parseOptDatetime)
+        "filter datetime."
+    ]
+
+-- TODO how to implement operators? --stress=1 >1 <1 !=1 <>1 >=1 <=1
+-- TODO better way to implement ordering? "<>"s needs escaping in most shells.
+-- TODO better way to implement ordering for datetimes? intervals?
+-- TODO more detailed descriptions
+
+-- TODO opts
+--  --name=expr : e.g. --name="agroup.*", --name="agroup.*.mytask", etc.
+--  --notname=expr : e.g. --notname "agroup.*.mytask"
 --  --stress op num : eg. stress=1, stress>0, etc.
 --  --datetime op datetime : e.g. datetime<'2014-01-01 00:00', etc.
+--
+--  unnecessary?:
+--   --with-complete : include completed tasks
+--   --without-complete : don't include completed tasks (default?)
 main = do
     args <- getArgs
     case length args of
